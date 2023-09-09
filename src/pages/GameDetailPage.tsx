3726c5e9d826175c71ab30ff425c5ecadbccaeb2
Md Christien <mdchristien12@gmail.com>
2023-09-09 10:53:16 +0100

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import CriticScore from "../components/CriticScore";
 import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import platforms from "../data/platforms";
 import useGame from "../hooks/useGame";
 
@@ -22,10 +23,10 @@ const GameDetailPage = () => {
           <ExpandableText>{game.description_raw}</ExpandableText>
           <GameAttributes game={game} />
         </GridItem>
-        {/* <GridItem>
+        <GridItem>
           <GameTrailer gameId={game.id} />
-          <GameScreenshots gameId={game.id} />
-        </GridItem> */}
+          {/* <GameScreenshots gameId={game.id} /> */}
+        </GridItem>
       </SimpleGrid>
     </>
   );
